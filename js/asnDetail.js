ba279708850f47122e4f358c691ff72e4cf566ea
lirui <liruiruilitwo@163.com>
2017-10-09 18:48:26 +0800

    nxx_second update

---
+++ js/asnDetail.js
@@ -0,0 +1,25 @@
+/**
+ * Created by lirui on 2017/10/9.
+ */
+(function () {
+    var SPA_RESOLVE_INIT = function (hashData) {
+
+        console.log('开始执行asnDetail.js');
+
+        asnDetail(hashData);
+
+    };
+
+    function asnDetail(hashData) {
+
+        // console.log(hashData);
+
+        // do something
+
+
+
+    }
+
+    return window.SPA_RESOLVE_INIT = SPA_RESOLVE_INIT
+
+})();
